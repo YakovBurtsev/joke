40acddda3601a4e4ee5f270365bd5b257a4f1971
yakov <yakovburtsev@gmail.com>
2019-07-15 19:59:47 +0300

    fix: create notification builder with channel id

--- app/src/main/java/com/example/joke/DelayedMessageService.java
+++ app/src/main/java/com/example/joke/DelayedMessageService.java
@@ -1,26 +1,36 @@
 package com.example.joke;
 
 import android.app.IntentService;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 
 
 public class DelayedMessageService extends IntentService {
 
     public static final String EXTRA_MESSAGE = "message";
     public static final int NOTIFICATION_ID = 5453;
+    private static final String CHANNEL_ID = "CHANNEL_001";
 
     public DelayedMessageService() {
         super("DelayedMessageService");
     }
 
     @Override
+    public void onCreate() {
+        super.onCreate();
+        createNotificationChannel();
+    }
+
+    @Override
     protected void onHandleIntent(Intent intent) {
         synchronized (this) {
             try {
-                wait(10_000);
+                wait(3_000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -31,22 +41,46 @@ public class DelayedMessageService extends IntentService {
     }
 
     private void showText(final String text) {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+        //Create a notification builder
+        Log.v("DelayedMessageService", "Create a notification builder");
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
                 .setSmallIcon(android.R.drawable.sym_def_app_icon)
                 .setContentTitle(getString(R.string.question))
                 .setContentText(text)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setVibrate(new long[]{0, 1000})
                 .setAutoCancel(true);
 
+        //Create an action
+        Log.v("DelayedMessageService", "Create an action");
         Intent actionIntent = new Intent(this, MainActivity.class);
         PendingIntent actionPendingIntent = PendingIntent.getActivity(
-                this, 0, actionIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+                this,
+                0,
+                actionIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         builder.setContentIntent(actionPendingIntent);
 
+        //Issue the notification
+        Log.v("DelayedMessageService", "Issue the notification");
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         notificationManager.notify(NOTIFICATION_ID, builder.build());
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 
 }
